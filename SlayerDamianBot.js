f4efdc80cf5d53c6b60dded018df02d9c56c7537
SlayerDamianBot <geeksenglish@gmail.com>
2014-11-07 14:42:57 -0200

    Update SlayerDamianBot.js

--- SlayerDamianBot.js
+++ SlayerDamianBot.js
@@ -961,6 +961,9 @@
                     API.sendChat(subChat(basicBot.chat.adfly, {name: chat.un}));
                     return true;
                 }
+                if(msg.indexOf(':eyeroll:') > -1){
+                    API.sendChat('/me ¬_¬');
+                }
                 if (msg.indexOf('autojoin was not enabled') > 0 || msg.indexOf('AFK message was not enabled') > 0 || msg.indexOf('!afkdisable') > 0 || msg.indexOf('!joindisable') > 0 || msg.indexOf('autojoin disabled') > 0 || msg.indexOf('AFK message disabled') > 0) {
                     API.moderateDeleteChat(chat.cid);
                     return true;
