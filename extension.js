299b3627b4a438f6725d2c7ff0a10674d18c25d9
SlayerDamianBot <geeksenglish@gmail.com>
2014-11-24 09:25:59 -0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -95,8 +95,8 @@
         fbLink: null,
         youtubeLink: null,
         website: null,
-        intervalMessages: ["Obrigado pela presença de todos! Divirtam-se =)", "A roleta acontece a cada 30 min, teste sua sorte para ganhar posições na fila!","Que tal colocar seus traseiros para remexer? Clique em legal :)"],
-        messageInterval: 3,
+        intervalMessages: ["Obrigado pela presença de todos! Divirtam-se =)", "A roleta acontece a cada 30 min, teste sua sorte para ganhar posições na fila!","Que tal colocar seus traseiros para remexer? Clique em legal :)","Oooooooooooooooooiii","xaaaaaaaaaaaauu"],
+        messageInterval: 1,
         songstats: true,
         commandLiteral: "!",
         blacklists: {
