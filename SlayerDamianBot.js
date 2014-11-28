1e2557a8ac19e8b3b62e8c74ef26155afeb11de0
SlayerDamianBot <geeksenglish@gmail.com>
2014-11-28 19:23:55 -0200

    Update SlayerDamianBot.js

--- SlayerDamianBot.js
+++ SlayerDamianBot.js
@@ -1420,6 +1420,19 @@
                     }
                 }
             },
+            
+            mehCommand: {
+                command: 'meh',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.meh);
+                    }
+                }
+            },
 
             baCommand: {
                 command: 'ba',
