8468e8d054e7293b6e968e14be2ec671324c2e0e
SlayerDamianBot <geeksenglish@gmail.com>
2014-12-02 22:50:42 -0200

    Update SlayerDamianBot.js

--- SlayerDamianBot.js
+++ SlayerDamianBot.js
@@ -2207,7 +2207,7 @@
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         var msg = chat.message;
-                        if (msg.length <= cmd.length + 1) return API.sendChat('/me MotD: ' + basicBot.settings.motd);
+                        if (msg.length <= cmd.length + 1) return API.sendChat('' + basicBot.settings.motd);
                         var argument = msg.substring(cmd.length + 1);
                         if (!basicBot.settings.motdEnabled) basicBot.settings.motdEnabled = !basicBot.settings.motdEnabled;
                         if (isNaN(argument)) {
